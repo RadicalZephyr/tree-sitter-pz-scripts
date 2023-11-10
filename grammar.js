90e06ebd9405b31c415c123d4388b88fc1af6a3f
Zefira Shannon <earthlingzephyr@gmail.com>
2023-11-10 05:57:28 -0800

    Sketch out grammar for pz-scripts

--- grammar.js
+++ grammar.js
@@ -2,6 +2,66 @@ module.exports = grammar({
   name: 'pz_scripts',
 
   rules: {
-    source_file: $ => 'hello'
+    source_file: $ => $._module,
+
+    _module: $ => seq(
+      'module',
+      $.identifier,
+      '{',
+      opt($.imports),
+      '}'
+    ),
+
+    imports: $ => seq(
+      'imports',
+      '{',
+      repeat($.identifier),
+      '}'
+    ),
+
+    identifier: $ => /\w+/,
+
+    number: $ => /\d+/,
+
+    item: $ => seq(
+      'item',
+      $.identifier,
+      '{',
+      repeat($.item_attribute),
+      '}'
+    ),
+
+    item_attribute: $ => seq(
+      $.identifier,
+      '=',
+      $.item_attr_value,
+      ','
+    ),
+
+    item_attr_value: $ => choice(
+      $.number,
+      $.identifier
+    ),
+
+    recipe: $ => seq(
+      'recipe',
+      $.recipe_name,
+      '{',
+      repeat1($.recipe_requirement),
+      repeat1($.recipe_attribute),
+      '}'
+    ),
+
+    recipe_attribute: $ => seq(
+      $.identifier,
+      ':',
+      $.recipe_attr_value,
+      ','
+    ),
+
+    recipe_attr_value: $ => choice(
+      $.number,
+      $.identifier,
+    ),
   }
 });
